55f92de04b21e6cbe65378a392183c7717699331
tmasuda <tomoyuki_masuda@agrex.co.jp>
2021-03-09 11:18:21 +0900

    ExcelDataReaderを使ってシートの一覧をクリップボードにコピーする。

--- ExcelSheetListMaker/Program.cs
+++ ExcelSheetListMaker/Program.cs
@@ -3,13 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Data;
+using ExcelDataReader;
+using System.Windows.Forms;
 
 namespace ExcelSheetListMaker
 {
     class Program
     {
+        [STAThreadAttribute]
         static void Main(string[] args)
         {
+            // コマンドライン引数からファイルパス、フォルダパスを取得する
+            var files = args.Where(x => File.Exists(x)).ToList();
+            var directories = args.Where(x => Directory.Exists(x)).ToList();
+            // フォルダパスから全てのファイルパスを取得する
+            var filesInSubdirecories = directories.SelectMany(x => Directory.EnumerateFiles(x, "*", SearchOption.AllDirectories)).ToList();
+            files.AddRange(filesInSubdirecories);
+
+            // 該当する拡張子のファイルパスを抽出する
+            string[] excelExtentions = { ".xlsx", ".xlsm", ".xlsb", ".xls", ".xls" };
+            var excelFiles = files.Where(x => excelExtentions.Contains(Path.GetExtension(x), StringComparer.OrdinalIgnoreCase));
+
+            if(excelFiles.ToList().Count == 0)
+            {
+                return;
+            }
+
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var file in excelFiles)
+            {
+                var ds = ReadExcelData(file);
+                if(ds == null)
+                {
+                    continue;
+                }
+
+                foreach(DataTable tbl in ds.Tables)
+                {
+                    sb.Append(file);
+                    sb.Append("\t");
+                    sb.Append(Path.GetDirectoryName(file));
+                    sb.Append("\t");
+                    sb.Append(Path.GetFileName(file));
+                    sb.Append("\t");
+                    sb.Append(tbl.TableName);
+                    sb.Append("\r\n");
+                }
+            }
+
+            Clipboard.SetText(sb.ToString());          
+        }
+
+        static DataSet ReadExcelData(string path)
+        {
+            DataSet ds = null;
+            try
+            {
+                using(var stream = File.Open(path, FileMode.Open, FileAccess.Read)){
+                    using(var reader = ExcelReaderFactory.CreateReader(stream))
+                    {
+                        ds = reader.AsDataSet();
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
+            }
+
+            return ds;
         }
     }
 }
